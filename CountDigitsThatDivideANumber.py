1d80f1e843730dfe2166ee4d2a04eaa577b03f2f
Aliya Fatima <aliyahassan032@gmail.com>
2023-11-11 22:34:38 +0530

    2520. Count the Digits That Divide a Number

---
+++ CountDigitsThatDivideANumber.py
@@ -0,0 +1,25 @@
+# 11-11-23
+# https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+
+
+class Solution: 
+    def count_digits(self, n):
+        x = n
+        count = 0
+        
+        while x != 0:
+            remainder = x % 10
+            x //= 10
+            
+            if remainder > 0 and n % remainder == 0:
+                count += 1
+        
+        return count
+    
+solution = Solution()
+print('The number of digits that divides this is: ', solution.count_digits(121))
+
+print('The number of digits that divides this is: ', solution.count_digits(1248))
+
+
+
