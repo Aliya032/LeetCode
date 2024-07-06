29698fec784c6e8b3451e5fb0fda5e792cd0de48
Aliya Fatima <aliyahassan032@gmail.com>
2024-07-06 19:47:18 +0530

    57. Insert Interval

---
+++ InsertInterval-57.py
@@ -0,0 +1,21 @@
+# 06-07-2024 19:16
+#https://www.youtube.com/watch?v=A8NUOmlwOlM
+# 
+class Solution: 
+    def insert(self, intervals: list[list[int]], newInterval: list[int]):
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        res.append(newInterval)
+       
+        return res
+    
+star = Solution()
+print(star.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
